1995ba88ede53f6fcec6a3bc7139e477d496af26
shawn <vpixel@163.com>
2025-11-01 23:09:55 +0800

    init

---
+++ array/array.h
@@ -0,0 +1,4 @@
+#ifndef _ARRAY_H_
+#define _ARRAY_H_
+void print_array(int arr[], int size);
+#endif // _ARRAY_H_
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "array.h"
+
+int main() {
+    printf("Hello, World!\n");
+    int arr[] = {1, 2, 3, 4, 5};
+    print_array(arr, 5);
+    return 0;
+}
